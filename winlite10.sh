d7f00c38311f849cd0ecf5b820de7f6c01a5a195
Rahmat Nanang <166177297+Rahmatnanang@users.noreply.github.com>
2024-04-05 16:30:18 -0700

    Update winlite10.sh

--- winlite10.sh
+++ winlite10.sh
@@ -11,7 +11,7 @@ echo Downloading Windows Disk...
 curl -L -o lite10.qcow2 https://bit.ly/32OIl8Y
 echo "Windows 10 x64 Lite On Gitpod, GCloud and GColab"
 echo Your VNC IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep this Tab to Open, Maximum Time 50h"
 echo "Support YT Channel-> Aank is ME"
