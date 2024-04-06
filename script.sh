b0763d4410e77cbe8634df11aa0062898bb8f28f
Rahmat Nanang <166177297+Rahmatnanang@users.noreply.github.com>
2024-04-05 20:03:07 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,7 +41,6 @@ apt-get install -y xfce4 xfce4-goodies > /dev/null 2>&1
 sudo apt install firefox -y > /dev/null 2>&1
 sudo apt-get install -y xrdp > /dev/null 2>&1
 sudo apt-get install -y xfce4-terminal
-sudo apt install firefox -y > /dev/null 2>&1
 sudo apt-get install chromium -y /dev/null 2>&1
 sudo service xrdp start
 sudo apt install dbus-x11
